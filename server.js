7994f21208e76860c89ea55dc9bd23205e15b41a
Kirk Roerig <mr.possoms@gmail.com>
2019-05-11 07:33:37 -0700

    Inital

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html');
+});
+
+var http = require('http').Server(app);
+
+var server = http.listen(process.env.PORT ? process.env.PORT : 3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://%s:%s', host, port);
+});
